3bb3dfda2ef51111955195ab67468d8ae9b6889d
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-07-18 18:06:07 +0530

    Add files via upload

---
+++ Java Basic Data Types.cpp
@@ -0,0 +1,22 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner input = new Scanner(System.in);
+		int testCase = input.nextInt();
+		while(testCase-- > 0)
+		{
+		    int in = input.nextInt();
+		    StringBuilder sbr = new StringBuilder(input.next());
+		    double d = input.nextDouble();
+		    System.out.print(in + " ");
+		    System.out.print(sbr+" ");
+		    System.out.println(d);
+		}
+	}
+}
