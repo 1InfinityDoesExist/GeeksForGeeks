10996f1f4e05e0fd9842370d6d28f719911591b9
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-07-19 14:31:15 +0530

    Add files via upload

---
+++ Upper case conversion.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    private:
+    public:
+        Solution();
+        void firstLetterCapital(string str);
+};
+Solution::Solution(){}
+void Solution::firstLetterCapital(string str)
+{
+    string s = "";
+    queue<string> q;
+    for(string::size_type iter = 0; iter < str.length(); iter++)
+    {
+        if(str[iter] == ' ')
+        {
+            if(s.length() == 0)
+            {
+                q.push("-1");
+                continue;
+            }
+            else
+            {
+                s[0] = toupper(s[0]);
+                q.push(s);
+                s = "";
+            }
+        }
+        else
+        {
+            s = s + str[iter];
+            if(iter == str.length()-1)
+            {
+                s[0] = toupper(s[0]);
+                q.push(s);
+            }
+        }
+    }
+    while(!q.empty())
+    {
+         if(q.front() == "-1" && q.size() == 1)
+        {
+            return;
+        }
+        if(q.size() == 1)
+        {
+            cout << q.front();
+            return;
+        }
+
+        if(q.front() == "-1")
+        {
+            cout << " ";
+            q.pop();
+        }
+        else
+        {
+            cout << q.front()<<" ";
+           q.pop();
+        }
+    }
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution sol;
+    int testCase;
+    cin >> testCase;
+    cin.ignore();
+    while(testCase--)
+    {
+        string str;
+        getline(cin, str);
+        sol.firstLetterCapital(str);
+        cout << endl;
+    }
+    return 0;
+}
