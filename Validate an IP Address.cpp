1990f62c54c2f03e32b037082adfde6d232e71ca
Avinash Patel <34315324+1InfinityDoesExist@users.noreply.github.com>
2018-07-27 17:16:43 +0530

    Add files via upload

--- Validate an IP Address.cpp
+++ Validate an IP Address.cpp
@@ -1,82 +1,96 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-class Solution
 {
-private:
-public:
-    Solution();
-    bool validIporNot(string str);
-};
-Solution::Solution(){}
+#include <bits/stdc++.h>
+using namespace std;
+int isValid(char *);
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		char s[10000];
+		cin>>s;
+		cout<<isValid(s)<<endl;
+	}
+	return 0;
+}
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* The function returns 1 if
+IP string is valid else return 0
+You are required to complete this method */
 bool isValid(string str)
 {
-    int num = stoi(str);
-    if(num >= 0 && num <= 255)
+    if(str.length() > 3 || stoi(str) > 255 || stoi(str) < 0)
     {
-        return true;
+        return false;
     }
-    return false;
+    if(str.length() > 1 && str[0] == '0')
+    {
+        return false;
+    }
+    return true;
 }
-bool Solution::validIporNot(string str)
+int isValid(char *ip)
 {
-    int count (0);
-    string s = "";
-    for(string::size_type iter = 0; iter < str.length(); iter++)
-    {
-        if(str[iter] == '.')
-        {
-            if(s.length() == 0)
-            {
-                continue;
-            }
-            else
-            {
-                if(isValid(s))
-                {
-                    count++;
-                    s = "";
-                }
-            }
-        }
-        else
-        {
-            s = s + str[iter];
-            if(iter == str.length()-1)
-            {
-                if(isValid(s))
-                {
-                    count++;
-                }
-            }
-        }
-    }
-    if(count == 4)
-    {
-        return true;
-    }
-    return false;
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    Solution sol;
-    int testCase;
-    cin >> testCase;
-    while(testCase--)
-    {
-        string str;
-        cin >> str;
-        bool ans = sol.validIporNot(str);
-        if(ans)
-        {
-            cout << "Valid Ip Address" <<endl;
-        }
-        else
-        {
-            cout << "Not Valid Ip Address "<<endl;
-        }
-    }
-    return 0;
+      //your code here
+      string str = "";
+      for(int iter = 0; ip[iter] != '\0'; iter++)
+      {
+          str = str + ip[iter];
+      }
+
+      if(str[str.length()-1] == '.')
+      {
+          return 0;
+      }
+      stack<string> st;
+      string s = "";
+      for(string::size_type iter = 0; iter < str.length(); iter++)
+      {
+          if(str[iter] == '.')
+          {
+              if(s.length() == 0)
+              {
+                  return 0;
+              }
+              else
+              {
+                  st.push(s);
+                  s = "";
+              }
+          }
+          else
+          {
+              if(isdigit(str[iter]))
+              {
+                  s = s+str[iter];
+                  if(iter == str.length()-1)
+                  {
+                      st.push(s);
+                  }
+              }
+              else
+              {
+                  return 0;
+              }
+          }
+      }
+      if(st.size() < 4 || st.size() > 4)
+      {
+          return 0;
+      }
+      while(!st.empty())
+      {
+         if(!isValid(st.top()))
+         {
+             return 0;
+         }
+         st.pop();
+      }
+      return 1;
 }
